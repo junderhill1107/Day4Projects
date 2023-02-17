79c6a80c0cc7468fb19686dfcf0b027721d0d0a4
Underhill <JU9115@mh.mercyhealthcare.org>
2023-02-17 01:13:27 -0600

    Testing Menu functions

--- Day4Projects.py
+++ Day4Projects.py
@@ -1,37 +1,55 @@
-import random
-myRandInt = random.randint(1,10)
-myRandFloat = random.random()
+# Day4Projects
 
-names = ["Jon", "Jeremiah", "Michael", "Trei"]
-fruit = ["kiwi", "apples", "bananas"]
-list1 = [names, fruit]
-#print(list1)
-#print(list1[1][2])
+def whoPays():
+    import random
+    """
+    myRandInt = random.randint(0,1)
 
-r1 = [" ", " ", " "]
-r2 = [" ", " ", " "]
-r3 = [" ", " ", " "]
-map =[r1, r2, r3]
-print("*** T R E A S U R E   M A P ***")
-print("*******************************")
-print(f"{r1}\n{r2}\n{r3}")
-print("*******************************")
 
-validOptions = [11, 12, 13, 21, 22, 23, 31, 32, 33]
+    if myRandInt == 1:
+        print("Heads")
+    else:
+        print ("Tails")
+    """
 
-print("Where do you want to put the treasure?")
-print("Valid options: 11, 12, 13, 21, 22, 23, 31, 32, 33")
-position = int(input("Treasure position: "))
+    namesIn = input("Give me everybody's names, separated by a comma: ")
+    #print(namesIn)
 
-while position not in validOptions:
-    print("You entered an invalid option.")
-    position = int(input("Treasure position: "))
-else:
-    strPosition = str(position)
-    p1 = int(strPosition[0]) - 1
-    p2 = int(strPosition[1]) - 1
-    map[p1][p2] = "x"
-    print("\n'X' marks the spot.")
+    splitNames = namesIn.split(", ")
+    # print(splitNames)
+
+    randNameIndex = random.randint(0, len(splitNames) - 1)
+    print(splitNames[randNameIndex] + " will be paying for the meal.")
+
+    randChoice = random.choice(splitNames)
+    print("Random.choice :: " + randChoice + " will be paying for the meal.")
+
+
+def treasureMap():
+    r1 = [" ", " ", " "]
+    r2 = [" ", " ", " "]
+    r3 = [" ", " ", " "]
+    map =[r1, r2, r3]
+    print("*** T R E A S U R E   M A P ***")
     print("*******************************")
     print(f"{r1}\n{r2}\n{r3}")
     print("*******************************")
+
+    validOptions = [11, 12, 13, 21, 22, 23, 31, 32, 33]
+
+    print("Where do you want to put the treasure?")
+    print("Valid options: 11, 12, 13, 21, 22, 23, 31, 32, 33")
+    position = int(input("Treasure position: "))
+
+    while position not in validOptions:
+        print("You entered an invalid option.")
+        position = int(input("Treasure position: "))
+    else:
+        strPosition = str(position)
+        p1 = int(strPosition[0]) - 1
+        p2 = int(strPosition[1]) - 1
+        map[p1][p2] = "X"
+        print("\n'X' marks the spot.")
+        print("*******************************")
+        print(f"{r1}\n{r2}\n{r3}")
+        print("*******************************")
--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-import Day4Projects
+def printMenu():
+    print("\n*** M E N U ***")
+    print("********************************")
+    print("1. Who's paying for the meal?")
+    print("2. Treasure Map")
+    print("3. Rock, Paper, Sissors")
+    print("0. Exit")
+    print("********************************\n")
 
-print("*** M E N U ***")
-print("1. Who's paying for the meal?")
-print("2. Treasure Map")
-print("3. Rock, Paper, Sissors")
-print("********************************\n")
 
-validInput = [1, 2, 3]
-menuChoice = int(input("Enter the MENU choice number:"))
 
-while menuChoice not in validInput:
-    print("You entered an invalid entry.")
+while True:
+    printMenu()
     menuChoice = int(input("Enter the MENU choice number:"))
 
-if menuChoice == 1:
-    Day4Projects.whoPays()
-elif menuChoice == 2:
-    Day4Projects.treasureMap()
-elif menuChoice == 3:
-    print(f"{menuChoice}")
+    import Day4Projects
+    if menuChoice == 1:
+        Day4Projects.whoPays()
+        break
+    elif menuChoice == 2:
+        Day4Projects.treasureMap()
+        break
+    elif menuChoice == 3:
+        print(f"Menu Choice: {menuChoice}")
+    elif 0:
+        break
+
+
+
